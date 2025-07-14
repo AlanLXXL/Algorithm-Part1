c07003cecf24d70d0b2fb6320670b1c34685e739
scyxl16 <123753884+AlanLXXL@users.noreply.github.com>
2025-07-14 10:26:10 +0800

    Update first homework

---
+++ RandomWord.java
@@ -0,0 +1,26 @@
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        // Initialise words and index
+        String champion = null;
+        int i = 1;
+
+        // Loop through words
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            if (StdRandom.bernoulli(1.0 / i)) {
+                champion = word;
+            }
+            i++;
+        }
+
+        // Output the champion
+        if (champion != null) {
+            StdOut.println(champion);
+        }
+
+    }
+}
